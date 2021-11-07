ddac50f9313aff257a7905ab4cd3ce63adc47077
romanwar <36025810+romanwar@users.noreply.github.com>
2021-11-07 21:19:32 +0300

    Update TenMinWalk.java

--- TenMinWalk.java
+++ TenMinWalk.java
@@ -15,27 +15,87 @@ give you an empty array (that's not a walk, that's standing still!).
 */
 
 
+
 public class TenMinWalk{
 	public static void main(String[] args){
-		// для теста реализовать создание рандомного массива  ['n', 's', 'w', 'e']
+		// РґР»СЏ С‚РµСЃС‚Р° СЂРµР°Р»РёР·РѕРІР°С‚СЊ СЃРѕР·РґР°РЅРёРµ СЂР°РЅРґРѕРјРЅРѕРіРѕ РјР°СЃСЃРёРІР°  ['n', 's', 'w', 'e']
 		//char[] walk = new char[10];
-		char[] walk = {'n','s','n','s','n','s','n','s','n','s'};
-		if(isValid(walk)){
+		char[] walk1 = { 'n','w','n','w','n','w','n','w','n','w'};
+/*
+'n' - false
+'n','s' - false
+'n','s','n','s','n','s','n','s','n','s','n','s' -false
+'n','s','e','w','n','s','e','w','n','s','e','w','n','s','e','w' - false
+'n','s','n','s','n','s','n','s','n','n' -false
+'e','e','e','w','n','s','n','s','e','w' -false
+'n','e','n','e','n','e','n','e','n','e' -false
+'n','w','n','w','n','w','n','w','n','w' -false
+*/ 
+		if(isValid(walk1)){
 			System.out.println("True");
 		}else{
-			System.out.println("Fals");
-		}
+			System.out.println("False");
+		}	
+		
 
 	}
 	
-	 public static boolean isValid(char[] walk){
+	public static boolean isValid(char[] walk){
 		//some code
-		if(walk.length == 10){
-			return true;
-		}else{
+		if(walk.length != 10){
 			return false;
 		}
+		int[] walkNew = new int[walk.length];
+		for(int i = 0; i < walk.length; i++){
+			if(walk[i] == 'n' || walk[i] == 'e'){
+				walkNew[i] = 1;
+			} else{
+				walkNew[i] = -1;
+			}
+		}
+		
+		int summaY = 0;
+		int summaX = 0;
+		for(int i = 0; i < walkNew.length; i++){
+			//System.out.println(summaX + " : " + summaY);
+			//System.out.println(summaY);
+			if(walk[i] == 'n' || walk[i] == 's'){
+				summaY = summaY + walkNew[i];				
+			} else {
+				summaX = summaX + walkNew[i];
+			}
+			//summa = summa + walkNew[i];
+		}
+		System.out.println(summaX + " : " + summaY);
+		if(summaY == 0 && summaX == 0){
+			return true;
+		} else{
+			return false;
+		}
+
 		
     }
 	
+	
 }
+
+
+/*
+public class TenMinWalk {
+  public static boolean isValid(char[] walk) {
+    if (walk.length != 10) return false;
+    
+    int x = 0, y = 0;
+    for (char c: walk) {
+      switch (c) {
+        case 'n': y++; break;
+        case 's': y--; break;
+        case 'w': x++; break;
+        case 'e': x--; break;
+      }
+    }
+    
+    return x == 0 && y == 0;
+  }
+}
+*/
